0b88ecb9719ec1dde9c6f90e04ddfee7fce52e26
Ajay Mittapally <62715854+ajaymittapally@users.noreply.github.com>
2025-06-16 19:44:07 -0400

    Time: 3 ms (63.65%) | Memory: 65.3 MB (34.74%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+   let l = 0;
+   let r = 1;
+   let max_p = 0;
+   while (r<prices.length){
+    if(prices[l]<prices[r]){
+        let profit = prices[r]- prices[l]
+        max_p = Math.max(profit,max_p)
+    }else{
+        l=r
+    }
+    r+=1;
+   }
+   return max_p
+    
+};
