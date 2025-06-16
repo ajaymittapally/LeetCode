a7f27a56ff38881d5651727814a7e99890fbd4ef
Ajay Mittapally <62715854+ajaymittapally@users.noreply.github.com>
2025-06-16 13:29:57 -0400

    Time: 0 ms (100.00%) | Memory: 53.2 MB (75.51%) - LeetSync

--- 66-plus-one/plus-one.js
+++ 66-plus-one/plus-one.js
@@ -3,17 +3,16 @@
  * @return {number[]}
  */
 var plusOne = function(digits) {
-let n = digits.length;
+    let n = digits.length;
 
-    for (let i = n - 1; i >= 0; i--) {
-        if (digits[i] < 9) {
-            digits[i] += 1;
+    for(let i =n-1;i>=0;i--){
+        if(digits[i] < 9){
+            digits[i] +=1
             return digits;
         }
-        digits[i] = 0;
+        digits[i] = 0
     }
-
-   
     digits.unshift(1);
-    return digits;
+    return digits
+
 };
