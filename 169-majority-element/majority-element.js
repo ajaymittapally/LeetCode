3e774f72d47ab7ce50f637b1cb0a14d057c6d31f
Ajay Mittapally <62715854+ajaymittapally@users.noreply.github.com>
2025-06-30 14:21:16 -0400

    Time: 6 ms (37.42%) | Memory: 57.1 MB (58.01%) - LeetSync

--- 169-majority-element/majority-element.js
+++ 169-majority-element/majority-element.js
@@ -3,18 +3,11 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    let candidate = null;
-    let count = 0
-    for(let i =0;i<nums.length;i++){
-        if(count ===0 ){
-            candidate = nums[i] 
+    let map ={}
+    for (let num of nums){
+        map[num] = (map[num] || 0) + 1
+        if ( map[num] > Math.floor(nums.length / 2)){
+            return num 
         }
-        if(candidate == nums[i]){
-            count +=1
-        }else{
-            count -=1
-        }
-        
     }
-    return candidate
 };
