a696d85a5e990489986bae765b538f4d819356cc
Ajay Mittapally <62715854+ajaymittapally@users.noreply.github.com>
2025-06-30 19:29:36 -0400

    Time: 4 ms (79.84%) | Memory: 62.5 MB (15.95%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.js
+++ 229-majority-element-ii/majority-element-ii.js
@@ -1,31 +1,36 @@
 var majorityElement = function(nums) {
-    let can1 = 0, can2 = 0;
-    let count1 = 0, count2 = 0;
+    let can1 = 0;
+    let can2 = 0;
+    let count1 = 0;
+    let count2 = 0;
 
-    for (let num of nums) {
-        if (num === can1) {
-            count1++;
-        } else if (num === can2) {
-            count2++;
-        } else if (count1 === 0) {
-            can1 = num;
-            count1 = 1;
-        } else if (count2 === 0) {
-            can2 = num;
-            count2 = 1;
-        } else {
-            count1--;
-            count2--;
+    for(let num of nums){
+        if(num === can1){
+            count1 +=1
+        }
+        else if(num === can2){
+            count2 +=1
+        }else if(count1 === 0){
+            can1 = num 
+            count1 = 1
+        }
+        else if( count2 === 0){
+            can2 = num
+            count2 = 1
+        }
+        else{
+            count1 -=1
+            count2 -=1
         }
     }
 
-    let res = [];
-    if (nums.filter(n => n === can1).length > Math.floor(nums.length / 3)) {
-        res.push(can1);
+    let res = []
+    let threshold = Math.floor(nums.length/3)
+    if( nums.filter(n=>n === can1).length > threshold){
+        res.push(can1)
     }
-    if (can2 !== can1 && nums.filter(n => n === can2).length > Math.floor(nums.length / 3)) {
-        res.push(can2);
+    if( can2 !== can1 && nums.filter(n=>n === can2).length > threshold){
+        res.push(can2)
     }
-
-    return res;
+    return res
 };
