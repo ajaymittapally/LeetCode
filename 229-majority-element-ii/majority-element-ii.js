07979ef37223170d84d9dc1ab4b71ecd298f4051
Ajay Mittapally <62715854+ajaymittapally@users.noreply.github.com>
2025-06-30 19:21:43 -0400

    Time: 12 ms (23.87%) | Memory: 62.6 MB (15.95%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.js
+++ 229-majority-element-ii/majority-element-ii.js
@@ -1,16 +1,31 @@
-/**
- * @param {number[]} nums
- * @return {number[]}
- */
 var majorityElement = function(nums) {
-    let map = {}
-    let res = []
-    for (let num of nums){
-        map[num] = (map[num]|| 0) +1
-        
-        if (map[num]>Math.floor(nums.length / 3) && !res.includes(num)){
-            res.push(Number(num))
+    let can1 = 0, can2 = 0;
+    let count1 = 0, count2 = 0;
+
+    for (let num of nums) {
+        if (num === can1) {
+            count1++;
+        } else if (num === can2) {
+            count2++;
+        } else if (count1 === 0) {
+            can1 = num;
+            count1 = 1;
+        } else if (count2 === 0) {
+            can2 = num;
+            count2 = 1;
+        } else {
+            count1--;
+            count2--;
         }
     }
-    return res
+
+    let res = [];
+    if (nums.filter(n => n === can1).length > Math.floor(nums.length / 3)) {
+        res.push(can1);
+    }
+    if (can2 !== can1 && nums.filter(n => n === can2).length > Math.floor(nums.length / 3)) {
+        res.push(can2);
+    }
+
+    return res;
 };
