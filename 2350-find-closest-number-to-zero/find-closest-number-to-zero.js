fc92d2db47aef0b6a8c95262fe07ba279f2ef685
Ajay Mittapally <62715854+ajaymittapally@users.noreply.github.com>
2025-07-02 19:36:03 -0400

    Time: 0 ms (100.00%) | Memory: 57.8 MB (27.72%) - LeetSync

--- 2350-find-closest-number-to-zero/find-closest-number-to-zero.js
+++ 2350-find-closest-number-to-zero/find-closest-number-to-zero.js
@@ -5,10 +5,11 @@
 var findClosestNumber = function(nums) {
     let closest = nums[0]
 
-    for(let i =0;i<nums.length;i++){
+    for (let i =0;i<nums.length;i++){
         let abscurr = Math.abs(nums[i])
-        let absclose = Math.abs(closest)
-        if( abscurr < absclose || abscurr === absclose && nums[i]> closest){
+        let absclo = Math.abs(closest)
+
+        if(abscurr<absclo || abscurr === absclo && nums[i] > closest){
             closest = nums[i]
         }
     }
